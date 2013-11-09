64bacd7a0e62d66740a0cc7f31c5c9de68731728
= <kaven.feng@autodesk.com>
2013-11-09 20:53:06 +0800

    Add date

--- GatherBox/ChooseDateTimeView.m
+++ GatherBox/ChooseDateTimeView.m
@@ -8,6 +8,12 @@
 
 #import "ChooseDateTimeView.h"
 
+@interface ChooseDateTimeView ()
+{
+    UIDatePicker*   mDatePicker;
+}
+@end
+
 @implementation ChooseDateTimeView
 
 - (id)initWithFrame:(CGRect)frame
@@ -15,7 +21,27 @@
     self = [super initWithFrame:frame];
     if (self) {
         // Initialization code
-        self.backgroundColor = [UIColor yellowColor];
+    
+        self.backgroundColor = [UIColor whiteColor];
+        mDatePicker = [ [ UIDatePicker alloc] initWithFrame:CGRectMake(0.0,0.0,300,200)];
+        [self addSubview:mDatePicker];
+        
+        NSDate* minDate = [[NSDate alloc]initWithString:@"1900-01-01 00:00:00 -0500"];
+        NSDate* maxDate = [[NSDate alloc]initWithString:@"2099-01-01 00:00:00 -0500"];
+        
+        mDatePicker.minimumDate = minDate;
+        mDatePicker.maximumDate = maxDate;
+        mDatePicker.minuteInterval = 15;
+        
+        UIButton *btn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+        btn.frame = CGRectMake(80, 210, 150, 50);
+        [self addSubview:btn];
+        
+        [btn setTitle:@"Done" forState:UIControlStateNormal];
+        [btn setTitle:@"Done" forState:UIControlStateHighlighted];
+        [btn addTarget:self action:@selector(onOK:) forControlEvents:UIControlEventTouchUpInside];
+
+
     }
     return self;
 }
@@ -29,12 +55,17 @@
 }
 */
 
-- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
-{
+-(void)onOK:(id)sender{
     [UIView beginAnimations:@"hide" context:nil];
     [UIView setAnimationDuration:0.5];
     self.frame = CGRectMake(0, 600, 320, 480);
     [UIView commitAnimations];
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    NSDate* _date = mDatePicker.date;
+    
 }
 
 @end
